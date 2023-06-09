b0a23ad568094c272fcc2dae9b7f1864830e4d42
AsmitaSubh <saswain92@gmail.com>
2023-06-09 21:14:34 +0530

    secondCommit

---
+++ DefaultValue.java
@@ -0,0 +1,23 @@
+public class DefaultValue
+{
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+    static boolean bo;
+    static char ch;
+    static byte b;
+    static short s;
+    public static void main(String[] args) {
+        System.out.println("the default value of int is :" + i);
+        System.out.println("the default value of long is :" + l);
+        System.out.println("the default value of float is :" + f);
+        System.out.println("the default value of double is :" + d);
+        System.out.println("the default value of boolean is :" + bo);
+        System.out.println("the default value of char is :" + ch);
+        System.out.println("the default value of byte is :" + b);
+        System.out.println("the default value of short is :" + s);
+
+
+    }
+}
