03e8eeb771d522fdbf7ae732e0f920eb56db84c2
AsmitaSubh <saswain92@gmail.com>
2023-06-10 13:11:38 +0530

    StringEqual

---
+++ StringEqual.java
@@ -0,0 +1,10 @@
+public class stringEqual {
+           public static void main(String args[]) {
+            String s1 = "subh";
+            String s2 = "subh";
+               if(s1== s2)
+                   System.out.println("Equal");
+               else
+                   System.out.println("Not Equal");
+        }
+    }
