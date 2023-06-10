d810ff265c4eb5f52598645f15b3f7052615565c
AsmitaSubh <saswain92@gmail.com>
2023-06-10 17:18:13 +0530

    InvalidCount

---
+++ InvalidCount.java
@@ -0,0 +1,20 @@
+public class InvalidCount
+{
+    public static void main(String[] args)
+    {
+            int sum = 0;
+            int invalidCount = 0;
+            for (String arg : args) {
+                try {
+                    int number = Integer.parseInt(arg);
+                    sum += number;
+                } catch (NumberFormatException e) {
+                    invalidCount++;
+                }
+            }
+            System.out.println("Sum of valid integers: " + sum);
+            System.out.println("Invalid integers entered: " + invalidCount);
+        }
+    }
+
+
